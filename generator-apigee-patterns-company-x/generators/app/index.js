fd9c1737873645806cc472bb052f74d0a9d4aa9c
Ruben Gonzalez <gonzalezruben@gonzalezruben-macbookpro.roam.corp.google.com>
2019-10-14 09:12:36 -0500

    migration to hostet targets

--- generator-apigee-patterns-company-x/generators/app/index.js
+++ generator-apigee-patterns-company-x/generators/app/index.js
@@ -9,106 +9,95 @@ var jsf = require('json-schema-faker');
 var apigeetool = require('apigeetool');
 var sdk = apigeetool.getPromiseSDK();
 var isWin = process.platform.startsWith('win');
+var supportedVerbs = ['GET','POST','PUT','DELETE','PATCH'];//TODO: HEAD and OPTIONS
 
 module.exports = class extends Generator {
-    
+
     async prompting() {
-        
-    this.log(require('yosay')('This is a sample implementation for a basic scaffolding tool.'));
-    this.log(chalk.yellow(
-        'Remember:'
-    ));
-    this.log(chalk.yellow('1. Your API is the first user interface of your application'));
-    this.log(chalk.yellow('2. Your API comes first, then the implementation'));
-    this.log(chalk.yellow('3. Your API should be self-descriptive'));
-    
-    this.promptAnswers = await this.prompt([{
-      type    : 'input',
-      name    : 'name',
-      message : 'Your API name',
-	default : this.appname, // Default to current folder name. This should also be your openAPI file name.
-	validate(input) {
-        return new Promise((resolve, reject) => {
-            SwaggerParser.validate(input+'.yaml').then((api) => {
-                resolve(true);
-            }).catch((err) => {
-		console.log(err);
-                resolve('You must provide an existing OpenAPI spec (yaml file in working directory) and the spec MUST be valid');
-            });
-        });
-	}
-    }, {
-        type : 'confirm',
-        name : 'createMock',
-        message : 'Would you like to create a mock service as the backend for your API?',
-        default : false
-    }, {
-        type : 'confirm',
-        name : 'publishApi',
-        message : 'Would your like to publish your API?',
-        default : false
-    }, {
-        type : 'input',
-        name : 'edgeUsername',
-        message : 'Please provide your Edge username.',
-        validate: (email) => {
-            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return (re.test(String(email).toLowerCase())) ? true : 'You must provide a valid Edge username (email)'; 
+
+      this.log(require('yosay')('Scaffolding tool for APIs deployed in Apigee Edge'));
+      this.log(chalk.yellow(
+          'Remember:'
+      ));
+      this.log(chalk.yellow('1. Your API is the first user interface of your application'));
+      this.log(chalk.yellow('2. Your API comes first, then the implementation'));
+      this.log(chalk.yellow('3. Your API should be self-descriptive'));
+
+      this.promptAnswers = await this.prompt([{
+          type : 'input',
+          name : 'name',
+          message : 'Your API name',
+  	      default : this.appname, // Default to current folder name. This should also be your openAPI file name.
+  	      validate(input) {
+              return new Promise((resolve, reject) => {
+                  SwaggerParser.validate(input+'.yaml').then((api) => {
+                      resolve(true);
+                  }).catch((err) => {
+  		                console.log(err);
+                      resolve('You must provide an existing OpenAPI spec (yaml file in working directory) and the spec MUST be valid');
+                  });
+              });
+          }
+      }, {
+          type : 'confirm',
+          name : 'createMock',
+          message : 'Would you like to create a mock service as the backend for your API?',
+          default : false
+      }, {
+          type : 'confirm',
+          name : 'publishApi',
+          message : 'Would your like to publish your API?',
+          default : false
+      }, {
+          type : 'input',
+          name : 'edgeUsername',
+          message : 'Please provide your Edge username.',
+          validate: (email) => {
+              var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+              return (re.test(String(email).toLowerCase())) ? true : 'You must provide a valid Edge username (email)';
+          },
+          when : (answers) => {
+              return answers.publishApi;
+          }
+      }, {
+          type : 'input',
+          name : 'edgeOrg',
+          message : 'Please provide your Edge organization name.',
+          validate: (input) => {
+              return (input && input !== "") ? true : 'You must provide a valid organization name';
         },
-        when : (answers) => {
-            return answers.publishApi;
-        }
-    }, {
-        type : 'input',
-        name : 'edgeOrg',
-        message : 'Please provide your Edge organization name.',
-        validate: (input) => {
-            return (input && input !== "") ? true : 'You must provide a valid organization name'; 
-        },
-        when : (answers) => {
-            return answers.publishApi;
-        }
-    },{
-        type : 'input',
-        name : 'edgeEnv',
-        message : 'Please provide your Edge environment name.',
-        validate: (input) => {
-            return (input && input !== "") ? true : 'You must provide a valid environment name'; 
-        },
-        when : (answers) => {
-            return answers.publishApi;
-        }
-    }, {
-        type : 'password',
-        name : 'edgePassword',
-        message : 'Please provide your Edge password.',
-        validate: (input) => {
-            return (input && input !== "") ? true : 'You must provide a valid password';
-        },
-        when : function(answers){
-            return answers.publishApi;
-        }
-    }
-                        //{
-        //type: 'list',
-        //name: "security",
-        //message: "Which security pattern are you going to use?",
-        //choices: ['B2B','B2C']
-        //             }
-        ]);
-		
-        //this.fs.copyTpl(
-        //    this.templatePath('flow-callout-OAuth.xml'),
-        //    this.destinationPath(answers.name+'/apiproxy/policies/flow-callout-OAuth-'+answers.security+'.xml'),
-        //    { security: answers.security }
-        //);
+          when : (answers) => {
+              return answers.publishApi;
+          }
+      }, {
+          type : 'input',
+          name : 'edgeEnv',
+          message : 'Please provide your Edge environment name.',
+          validate: (input) => {
+              return (input && input !== "") ? true : 'You must provide a valid environment name';
+          },
+          when : (answers) => {
+              return answers.publishApi;
+          }
+      }, {
+          type : 'password',
+          name : 'edgePassword',
+          message : 'Please provide your Edge password.',
+          validate: (input) => {
+              return (input && input !== "") ? true : 'You must provide a valid password';
+          },
+          when : function(answers){
+              return answers.publishApi;
+          }
+      }
+      ]);
     }
 
     openapiToApigee(){
-		this.log(chalk.yellow('Creating API Proxy bundle...'));
-		this.spawnCommandSync('openapi2apigee',
-	                  ['generateApi', this.promptAnswers.name, '-s', this.promptAnswers.name+'.yaml', '-d', '.']);
-	    this.log(chalk.yellow('API Proxy bundle created!'));
+		    this.log(chalk.yellow('Creating API Proxy bundle...'));
+		    this.spawnCommandSync('openapi2apigee',
+          ['generateApi', this.promptAnswers.name, '-s', this.promptAnswers.name+'.yaml', '-d', '.']);
+	      this.log(chalk.yellow('API Proxy bundle created!'));
     }
 
     deleteZipFile(){
@@ -116,13 +105,14 @@ module.exports = class extends Generator {
 		fsy.unlinkSync('./'+this.promptAnswers.name+'/apiproxy.zip');
 
     }
+
     copyPomTemplate(){
-	this.fs.copyTpl(
-	    this.templatePath('pom.xml'),
-	    this.destinationPath(this.promptAnswers.name+'/pom.xml'),
-	    {api_name : this.promptAnswers.name}
-	);
-	this.fs.commit(()=>{});
+	     this.fs.copyTpl(
+	        this.templatePath('pom.xml'),
+	        this.destinationPath(this.promptAnswers.name+'/pom.xml'),
+	        {api_name : this.promptAnswers.name}
+	     );
+	     this.fs.commit(()=>{});
     }
 
     setBasePath(){
@@ -130,11 +120,11 @@ module.exports = class extends Generator {
             SwaggerParser.validate(this.promptAnswers.name+'.yaml').then((api) => {
                 this.apiProduces = api.produces;
                 this.apiConsumes = api.consumes;
-                this.basePath = api.basePath;	
+                this.basePath = api.basePath;
                 let srcDocument = this.fs.read(this.promptAnswers.name + '/apiproxy/proxies/default.xml');
-				let doc = new dom().parseFromString(srcDocument);
-				let nodes = xpath.select("/ProxyEndpoint/HTTPProxyConnection/BasePath", doc);
-				nodes[0].textContent = api.basePath;
+				        let doc = new dom().parseFromString(srcDocument);
+				        let nodes = xpath.select("/ProxyEndpoint/HTTPProxyConnection/BasePath", doc);
+				        nodes[0].textContent = api.basePath;
                 this.fs.write(this.promptAnswers.name + '/apiproxy/proxies/default.xml', doc.toString());
                 this.fs.commit(()=>{});
                 resolve(true);
@@ -143,30 +133,26 @@ module.exports = class extends Generator {
     }
 
     createMock(){
-		if(this.promptAnswers.createMock){
-			fsy.copySync(this.templatePath('node'), this.promptAnswers.name + '/node');
-			execSync('cd '+this.promptAnswers.name+'/node && npm install');
-			let srcDocument = this.fs.read(this.promptAnswers.name + '/apiproxy/targets/default.xml');
-			let doc = new dom().parseFromString(srcDocument);
-			let nodes = xpath.select("/TargetEndpoint/HTTPTargetConnection", doc);
-			doc.removeChild(nodes[0]);
-			let target = xpath.select("/TargetEndpoint", doc);
-			let scriptTarget = doc.createElement('ScriptTarget');
-			let resourceURL = doc.createElement('ResourceURL');
-			resourceURL.textContent = 'node://app.js';
-			scriptTarget.appendChild(resourceURL);
-			target[0].appendChild(scriptTarget);
-			this.fs.write(this.promptAnswers.name + '/apiproxy/targets/default.xml', doc.toString());
-			this.fs.commit(()=>{});
-		}
-		this.log(chalk.yellow('Templates rendered!'));
+  		if(this.promptAnswers.createMock){
+  			fsy.copySync(this.templatePath('node'), this.promptAnswers.name + '/node');
+  			let srcDocument = this.fs.read(this.promptAnswers.name + '/apiproxy/targets/default.xml');
+  			let doc = new dom().parseFromString(srcDocument);
+  			let nodes = xpath.select("/TargetEndpoint/HTTPTargetConnection", doc);
+  			doc.removeChild(nodes[0]);
+  			let target = xpath.select("/TargetEndpoint", doc);
+  			let hostedTarget = doc.createElement('HostedTarget');
+  			target[0].appendChild(hostedTarget);
+  			this.fs.write(this.promptAnswers.name + '/apiproxy/targets/default.xml', doc.toString());
+  			this.fs.commit(()=>{});
+  		}
+  		this.log(chalk.yellow('Templates rendered!'));
     }
 
     async createMockServer(){
         if(this.promptAnswers.createMock){
-		this.log(chalk.yellow('Creating NodeJS Mock server...'));
-        return await new Promise((resolve, reject) => {
-           let nativeObject = SwaggerParser.dereference(this.promptAnswers.name+'.yaml');
+		        this.log(chalk.yellow('Creating NodeJS Mock server...'));
+            return await new Promise((resolve, reject) => {
+            let nativeObject = SwaggerParser.dereference(this.promptAnswers.name+'.yaml');
             let mockConfig = new Object();;
             mockConfig.mockDirectory = './mock';
             mockConfig.quiet = false;
@@ -174,7 +160,6 @@ module.exports = class extends Generator {
             mockConfig.latency = 50;
             mockConfig.logRequestHeaders = false;
             let webServices = new Object();
-            let supportedVerbs = ['GET','POST','PUT','DELETE','HEAD','OPTIONS','PATCH'];
             let resolveSchema = (schema) => {
                 jsf.option({
                     alwaysFakeOptionals: true
@@ -187,10 +172,10 @@ module.exports = class extends Generator {
                         verbs.push(verb);
                         let useJsonSchemas = () => {
                             if(path[verb].produces){
-                            return path[verb].produces.includes('application/json');
+                              return path[verb].produces.includes('application/json');
                             }
                             if(this.apiProduces){
-                            return this.apiProduces.includes('application/json');
+                              return this.apiProduces.includes('application/json');
                             }
                         };
                         if(useJsonSchemas() && path[verb].responses){
@@ -255,22 +240,17 @@ module.exports = class extends Generator {
             });
         }
     }
-	
+
 	copyNodeResources(){
 		if(this.promptAnswers.createMock){
-			if(isWin){
-				execSync('cd '+this.promptAnswers.name+'/node && powershell.exe -nologo -noprofile -command "& { Add-Type -A \'System.IO.Compression.FileSystem\'; [IO.Compression.ZipFile]::CreateFromDirectory(\'node_modules\', \'node_modules.zip\'); }" && rmdir /s /q node_modules');
-			} else {
-				execSync('cd '+this.promptAnswers.name+'/node && zip -r node_modules.zip node_modules/ && rm -r node_modules');
-			}
-			fsy.copySync(this.promptAnswers.name + '/node', this.promptAnswers.name + '/apiproxy/resources/node');
+			fsy.copySync(this.promptAnswers.name + '/node', this.promptAnswers.name + '/apiproxy/resources/hosted');
 			this.log(chalk.yellow('Mock server created!'));
 		}
 	}
 
     createTests(){
 		if(this.promptAnswers.createMock){
-		this.log(chalk.yellow('Rendering Apickli test templates...'));
+		    this.log(chalk.yellow('Rendering Apickli test templates...'));
             return new Promise((resolve, reject) => {
                 let parameterMap = new Object();
                 let resolveSchema = (schema) => {
@@ -281,12 +261,12 @@ module.exports = class extends Generator {
                 };
                 let evalVerb = (pathString, path, verb) => {
                     return new Promise((resolve, reject) => {
-                        if(verb.toUpperCase() === 'POST' || verb.toUpperCase() === 'PUT' || verb.toUpperCase() === 'GET' || verb.toUpperCase() === 'PATCH' || verb.toUpperCase() === 'DELETE'){
+                        if(supportedVerbs.includes(verb.toUpperCase())){
                             parameterMap[pathString+verb] = new Object();
-							parameterMap[pathString+verb].body = new Array();
-							parameterMap[pathString+verb].query = new Array();
-							parameterMap[pathString+verb].path = new Array();
-							let useJsonSchemas = () => {
+              							parameterMap[pathString+verb].body = new Array();
+              							parameterMap[pathString+verb].query = new Array();
+              							parameterMap[pathString+verb].path = new Array();
+							              let useJsonSchemas = () => {
                                 if(path[verb].consumes){
                                     return path[verb].consumes.includes('application/json');
                                 }
@@ -343,14 +323,14 @@ module.exports = class extends Generator {
                 }));
                 };
                 evalPaths(this.apiDereferenced).then((resolved) => {
-					
+
 					fsy.copySync(this.templatePath('tests'), this.promptAnswers.name + '/tests');
-					let replacePathParams = (valArray, path) => {
-						for(let val in valArray){
-							path = path.replace(new RegExp('{'+valArray[val].name+'}','g'),valArray[val].val);
-						}
-						return path;
-					};
+  					let replacePathParams = (valArray, path) => {
+  						for(let val in valArray){
+  							path = path.replace(new RegExp('{'+valArray[val].name+'}','g'),valArray[val].val);
+  						}
+  						return path;
+  					};
                     this.fs.copyTpl(
                         this.templatePath('sampleFeature.feature'),
                         this.destinationPath(this.promptAnswers.name+'/tests/features/sampleFeature.feature'),
@@ -362,7 +342,7 @@ module.exports = class extends Generator {
             });
         }
     }
-    
+
     async publishApi(){
 		this.log(chalk.yellow('Test templates rendered!'));
 		if(this.promptAnswers.publishApi){
@@ -376,11 +356,11 @@ module.exports = class extends Generator {
 				directory:'./'+this.promptAnswers.name,
 				debug: false
 			}
-			
+
 			await sdk.deployProxy(opts);
 		}
     }
-	
+
 	testApi(){
 		this.log(chalk.yellow('API Proxy bundle deployed!'));
 		if(this.promptAnswers.publishApi){
@@ -390,7 +370,7 @@ module.exports = class extends Generator {
 				let result = execSync( '\"node_modules/.bin/cucumberjs.cmd\" \"features/\" --world-parameters \"{\\"proxyEndpoint\\":\\"'+this.promptAnswers.edgeOrg+'-test.apigee.net'+this.basePath+'\\"}\"',
 				{cwd:'./'+this.promptAnswers.name+'/tests'});
 				this.log(result.toString('utf8'));
-			} else {					
+			} else {
 				let result = execSync( './node_modules/.bin/cucumber.js ./features --world-parameters \'{"proxyEndpoint":"'+this.promptAnswers.edgeOrg+'-test.apigee.net'+this.basePath+'"}\'',
 				{cwd:'./'+this.promptAnswers.name+'/tests'});
 				this.log(result.toString('utf8'));
